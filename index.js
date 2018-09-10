78507a86310f0b8489106d76e5ae9b4ae34c8827
陳融威 <chenrongwei@chenrongweide-MacBook-Pro.local>
2018-09-10 09:28:09 +0800

    commit

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 
-//document.getElementById("btn1").addEventListener("click", AddRow());
-document.getElementById('btn1').onclick= function(){AddRow()};
+var Row_Cnt=0;
+var Row_Del=0;
+document.getElementById("Add_Btn").onclick=function(){AddRow()};
+var tb1=document.getElementById("tb1")
 function AddRow(){
-    document.getElementById("tb1").innerHTML+='<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>'
+    Row_Cnt+=1;
+    tb1.innerHTML+='<tr id="item'+Row_Cnt+'">'
+    +'<td>'+Row_Cnt+'</td>'
+    +'<td><input type="text" disabled></td>'
+    +'<td><button class="Edit_Btn" onclick="EditRow('+Row_Cnt+')">修改資料</button></td>'
+    +'<td><button class="Del_Btn" onclick="DelRow('+Row_Cnt+')">刪除資料</button></td></tr>';
+    //document.getElementsByClassName('Del_Btn').forEach.call.onclick=function(){DelRow()};
+}
+function DelRow(Row_Num){
+    document.getElementById("item"+Row_Num).remove();
+}
 
+function EditRow(Row_Num){
+    if(document.getElementById("item"+Row_Num).getElementsByTagName("button")[0].innerHTML=="修改資料")
+    {
+        document.getElementById("item"+Row_Num).getElementsByTagName("input")[0].disabled=false;
+        document.getElementById("item"+Row_Num).getElementsByTagName("button")[0].innerHTML='送出資料';
+    }
+    else
+    {
+        document.getElementById("item"+Row_Num).getElementsByTagName("input")[0].disabled=true;
+        document.getElementById("item"+Row_Num).getElementsByTagName("button")[0].innerHTML='修改資料';
+        Btn_Mode="Send";
+    }
 }
 
